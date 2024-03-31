b6335996ecd04664baa3fbd6fc7f608d31240956
Ishaan Buddharaju <ishaan.buddharaju@gmail.com>
2024-03-31 15:57:42 -0500

    Updated Data Frame to format correctly and output Grade within the Heatmap

--- Heatmap.py
+++ Heatmap.py
@@ -9,7 +9,14 @@ import seaborn as sns
 # pip install numpy
 
 Data = pd.read_csv('StudentData.csv')
-CorrelationDF = Data.drop(labels=["Student", "Grade"], axis=1).corr()
+
+Data['#AP/Honors'] = Data['#Honors semesters'] + Data['#AP semesters']
+Data = Data[['Student', 'Grade', 'GPA', '#AP/Honors', '#Honors semesters', '#AP semesters', 'Days Enrolled', 'Days Present', 'Att Rate']]
+Data['Days Absent'] = Data['Days Enrolled'] - Data['Days Present']
+Data = Data[['Student', 'Grade', 'GPA', '#AP/Honors', '#Honors semesters', '#AP semesters', 'Days Absent', 'Days Enrolled', 'Days Present', 'Att Rate']]
+Data.drop(labels=['#Honors semesters', '#AP semesters','Days Present'], axis=1)
+
+CorrelationDF = Data.drop(labels=["Student"], axis=1).corr()
 
 plt.figure(figsize=(16,6))
 FeatureHeatMap = sns.heatmap(CorrelationDF.corr(), vmin=-1, vmax=1, annot=True, cmap='BrBG')
