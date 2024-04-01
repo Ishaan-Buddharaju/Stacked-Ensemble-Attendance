261dd8ba4452e6376bbf08960c0d268d2e9f5a2f
Ishaan Buddharaju <ishaan.buddharaju@gmail.com>
2024-03-31 22:23:27 -0500

    Finalize Heatmap code, must name data csv "StudentData.csv"

--- Heatmap.py
+++ Heatmap.py
@@ -3,11 +3,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-# pip install seaborn
 # python -m pip install -U pip
 # python -m pip install -U matplotlib
 # pip install numpy
 # pip install pandas
+# pip install seaborn
+
 
 Data = pd.read_csv('StudentData.csv')
 
@@ -17,9 +18,9 @@ Data['Days Absent'] = Data['Days Enrolled'] - Data['Days Present']
 Data = Data[['Student', 'Grade', 'GPA', '#AP/Honors', '#Honors semesters', '#AP semesters', 'Days Absent', 'Days Enrolled', 'Days Present', 'Att Rate']]
 Data = Data.drop(labels=['#Honors semesters', '#AP semesters','Days Present'], axis=1)
 
-CorrelationDF = Data.drop(labels=["Student"], axis=1).corr()
+CorrelationDF = Data.drop(labels=["Student", "Att Rate"], axis=1).corr()
 
-plt.figure(figsize=(16,6))
+plt.figure(figsize=(20,10))
 FeatureHeatMap = sns.heatmap(CorrelationDF.corr(), vmin=-1, vmax=1, annot=True, cmap='BrBG')
 FeatureHeatMap.set_title('Feature Correlation Heatmap')
 plt.savefig(fname= 'Feature Correlation Heatmap')
